c4b977c25e2c4ddf4dac8c138540dadde976d517
Gaël <33gael.dev@gmail.com>
2025-11-20 09:54:09 +0100

    includes

--- src/minitalk.h
+++ src/minitalk.h
@@ -1,11 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.c                                         :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gaeducas <gaeducas@student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/20 09:49:13 by gaeducas          #+#    #+#             */
-/*   Updated: 2025/11/20 09:53:53 by gaeducas         ###   ########.fr       */
+/*   Created: 2025/11/20 09:49:11 by gaeducas          #+#    #+#             */
+/*   Updated: 2025/11/20 09:53:40 by gaeducas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../includes/ft_printf/ft_printf.h"
+# include "../includes/libft/libft.h"
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
