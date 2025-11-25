fa4545460dce70097c8f521daf60252fbb3cd266
Gaël <33gael.dev@gmail.com>
2025-11-25 17:47:43 +0100

    pull linux 42

--- src/server.c
+++ src/server.c
@@ -6,13 +6,21 @@
 /*   By: gaeducas <gaeducas@student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 09:44:44 by gaeducas          #+#    #+#             */
-/*   Updated: 2025/11/24 16:26:41 by gaeducas         ###   ########.fr       */
+/*   Updated: 2025/11/25 17:45:49 by gaeducas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	signal_handler(int signum)
+{
+	ft_printf("%d", "Received SIGINT !\n", signum);
+	exit(0);
+}
+
 int	main(void)
 {
 	ft_printf("%d", getgid());
+	signal(SIGINT, signal_handler);
+	signal(SIGTERM, signal_handler);
 }
