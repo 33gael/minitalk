b0421235ccf7a3123a50aa312ace2b326b0e981d
Gaël <33gael.dev@gmail.com>
2025-12-02 16:41:12 +0100

    dsqbfjlsqdfdsf

--- src/client.c
+++ src/client.c
@@ -6,17 +6,22 @@
 /*   By: gaeducas <gaeducas@student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 09:44:40 by gaeducas          #+#    #+#             */
-/*   Updated: 2025/11/26 12:26:21 by gaeducas         ###   ########.fr       */
+/*   Updated: 2025/12/02 16:40:54 by gaeducas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(int PID, char **av)
+int	main(int argc, char **argv)
 {
-	int i;
+	int pid;
+	char *str;
 
-	i = 0;
-	if (PID != 3)
-		return (0);
-}
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		str = argv[2];
+		// envoyer le message
+	}
+	else
+		ft_printf("\nPlease write more than 1 character");
--- src/server.c
+++ src/server.c
@@ -6,24 +6,20 @@
 /*   By: gaeducas <gaeducas@student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 09:44:44 by gaeducas          #+#    #+#             */
-/*   Updated: 2025/11/26 13:30:32 by gaeducas         ###   ########.fr       */
+/*   Updated: 2025/12/02 16:30:28 by gaeducas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	signal_handler(int signum, siginfo_t *signal_handler, void *use_siginfo)
+void	signal_handler(int signum)
 {
 	exit(0);
 }
 
 int	main(void)
 {
-	ft_printf("%d", "✅ Received SIGINT !\n", getpid());
-	ft_signal(SIGUSR1, signal_handler, 1);
-	ft_signal(SIGUSR2, signal_handler, 1);
+	ft_printf("%s : %d", "✅ PID Received ! ", getpid());
 	while (1)
-	{
 		pause();
-	}
 }
